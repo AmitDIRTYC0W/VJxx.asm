2df1ec8f54debe7122373e27b6d2e4a80c5f4754
Amit Goren <animalfarm@mailfence.com>
2022-04-28 20:27:10 +0300

    Make the sum_area test independent of vjxx_integrate_image

--- VJxx/tests/sum_area.c
+++ VJxx/tests/sum_area.c
@@ -6,37 +6,29 @@
 #include "VJxx/integral_image.h"
 
 #define IN_VALUES { \
-    255,   0, 255,   0, 255, \
-      0, 255,   0, 255,   0, \
-    255,   0, 255,   0, 255 \
-  }
-#define IN_WIDTH 5
-#define IN_HEIGHT 3
-#define IN_SIZE IN_WIDTH * IN_HEIGHT
+    0,    0,    0,    0,    0,    0, \
+    0,  255,  255,  510,  510,  765, \
+    0,  255,  510, 765, 1020, 1275, \
+    0,  510,  765, 1275, 1530, 2040 \
+}
+#define IN_WIDTH 6
+#define IN_HEIGHT 4
+#define IN_SIZE IN_WIDTH * IN_HEIGHT * sizeof(unsigned int)
 
 #define EXPECTED_SUM 1020
 
 int main() {
-  struct vjxx_image picture;
-  picture.width = IN_WIDTH;
-  picture.height = IN_HEIGHT;
-  picture.values = malloc(IN_SIZE);
-  const char in_values[] = IN_VALUES;
-  memcpy(picture.values, in_values, IN_WIDTH * IN_HEIGHT);
-
   struct vjxx_integral_image integral_img;
-  if (vjxx_integrate_image(&integral_img, picture) > 0) {
-    return EXIT_FAILURE;
-  }
-
-  free(picture.values);
-
+  integral_img.width = IN_WIDTH;
+  integral_img.height = IN_HEIGHT;
+  const unsigned int in_values[] = IN_VALUES;
+  integral_img.values = in_values;
+  memcpy(integral_img.values, in_values, IN_WIDTH * IN_HEIGHT);
+  printf("\n\n%d\n\n", vjxx_sum_area(integral_img, 1, 1, 5, 3));
   if (vjxx_sum_area(integral_img, 1, 1, 5, 3) != EXPECTED_SUM) {
     return EXIT_FAILURE;
   }
-
-  free(integral_img.values);
-
+  
   return EXIT_SUCCESS;
 }
 
